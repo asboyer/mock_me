8e4e6634d9410fb990160793f55af51c524ab4b8
Andrew Boyer <asboyer@gmail.com>
2020-08-10 14:35:58 -0400

    added quotes to make it look better

--- speech.py
+++ speech.py
@@ -9,7 +9,7 @@ with sr.Microphone() as source:
     try:
         text = r.recognize_google(audio)
         #print(text)
-        print('You said: {0}'.format(text[0].upper()) + text[1:] + '.')
-
+        #print('You said: {0}'.format(text[0].upper()) + text[1:] + '.')
+        print("You said: " + '"' + text[0].upper() + text[1:] + '."')
     except:
         print("Sorry, could not recognize your voice")
