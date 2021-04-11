c0f4dfb0e133b0348ce45a920da3e1699202f12c
boyer <asboyer@gmail.com>
2021-04-10 22:54:41 -0400

    changed readme

--- speech.py
+++ speech.py
@@ -1,15 +1,17 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-
-with sr.Microphone() as source:
-    print("Say something:")
-    audio = r.listen(source)
-
-    try:
-        text = r.recognize_google(audio)
+while True:
+    with sr.Microphone() as source:
+        print("Say something:")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
         #print(text)
         #print('You said: {0}'.format(text[0].upper()) + text[1:] + '.')
-        print("You said: " + '"' + text[0].upper() + text[1:] + '."')
-    except:
-        print("Sorry, could not recognize your voice")
+            print("You said: " + '"' + text[0].upper() + text[1:] + '."')
+        except:
+            print("Sorry, could not recognize your voice")
+            continue
+        else:
+            break
